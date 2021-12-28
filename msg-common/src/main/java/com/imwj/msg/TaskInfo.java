ee6dd5b0dba210a0d3625c76301249be2e029f86
weixiaojian <2916863213@qq.com>
2021-12-28 11:19:05 +0800

    init

---
+++ msg-common/src/main/java/com/imwj/msg/TaskInfo.java
@@ -0,0 +1,82 @@
+package com.imwj.msg;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.swing.text.html.parser.ContentModel;
+import java.util.Set;
+
+/**
+ * @author langao_q
+ * @since 2021-12-28 11:07
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskInfo {
+
+    /**
+     * 消息模板Id
+     */
+    private Long messageTemplateId;
+
+    /**
+     * 业务Id(数据追踪使用)
+     * 生成逻辑参考
+     */
+    private Long businessId;
+
+    /**
+     * 接收者
+     */
+    private Set<String> receiver;
+
+    /**
+     * 发送的Id类型
+     */
+    private Integer idType;
+
+    /**
+     * 发送渠道
+     */
+    private Integer sendChannel;
+
+    /**
+     * 模板类型
+     */
+    private Integer templateType;
+
+    /**
+     * 消息类型
+     */
+    private Integer msgType;
+
+    /**
+     * 发送文案模型
+     * message_template表存储的content是JSON(所有内容都会塞进去)
+     * 不同的渠道要发送的内容不一样(比如发push会有img，而短信没有)
+     * 所以会有ContentModel
+     */
+    private ContentModel contentModel;
+
+    /**
+     * 发送账号（邮件下可有多个发送账号、短信可有多个发送账号..）
+     */
+    private Integer sendAccount;
+
+    /**
+     * 消息去重时间 单位小时
+     */
+    private Integer deduplicationTime;
+
+    /**
+     * 是否夜间屏蔽
+     * 0:不屏蔽
+     * 1：屏蔽
+     */
+    private Integer isNightShield;
+
+}
---
+++ msg-web/src/main/java/com/imwj/msg/MsgApplication.java
@@ -0,0 +1,17 @@
+package com.imwj.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author langao_q
+ * @since 2021-12-28 10:48
+ */
+@SpringBootApplication
+public class MsgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgApplication.class, args);
+    }
+
+}
